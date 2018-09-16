d124b8adfb1aa7f8b58852844cd4c9264383b762
Lennart Espe <lnsp@users.noreply.github.com>
2018-09-16 19:19:38 +0200

    Add support for HTTPS

--- cmd/koala/main.go
+++ cmd/koala/main.go
@@ -12,10 +12,12 @@ import (
 var version = "dev-build"
 
 type Specification struct {
-	Addr      string `default:":8080" description:"Address the server will be listening on"`
-	Zonefile  string `required:"true" description:"Zonefile to be edited"`
-	StaticDir string `default:"web/dist" description:"Directory of static assets"`
-	ApplyCmd  string `default:"sleep 1" description:"Command executed after applying zonefile changes"`
+	Addr        string `default:":8080" description:"Address the server will be listening on"`
+	Zonefile    string `required:"true" description:"Zonefile to be edited"`
+	StaticDir   string `default:"web/dist" description:"Directory of static assets"`
+	ApplyCmd    string `default:"sleep 1" description:"Command executed after applying zonefile changes"`
+	Certificate string `default:"" description:"Certificate file for HTTPS"`
+	PrivateKey  string `default:"" description:"Private key file for HTTPS"`
 }
 
 func main() {
@@ -32,8 +34,15 @@ func main() {
 			ApplyCmd:  strings.Split(s.ApplyCmd, " "),
 		}),
 	}
-	log.Printf("koala %s ready to serve!\n", version)
-	if err := srv.ListenAndServe(); err != nil {
-		log.Println("failed to serve:", err)
+	log.Printf("koala %s ready to serve!", version)
+	if s.Certificate != "" {
+		log.Printf("enabled HTTPS using cert '%s' and key '%s'", s.Certificate, s.PrivateKey)
+		if err := srv.ListenAndServeTLS(s.Certificate, s.PrivateKey); err != nil {
+			log.Println("failed to serve:", err)
+		}
+	} else {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println("failed to serve:", err)
+		}
 	}
 }
