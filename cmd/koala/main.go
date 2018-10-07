fe0e0ca7e0f34cd290b6214fbdd2e141b3789de9
Lennart Espe <lnsp@users.noreply.github.com>
2018-10-07 20:49:35 +0200

    Remove staticdir env configuration

--- cmd/koala/main.go
+++ cmd/koala/main.go
@@ -14,7 +14,6 @@ var version = "dev-build"
 type Specification struct {
 	Addr        string `default:":8080" description:"Address the server will be listening on"`
 	Zonefile    string `required:"true" description:"Zonefile to be edited"`
-	StaticDir   string `default:"web/dist" description:"Directory of static assets"`
 	ApplyCmd    string `default:"sleep 1" description:"Command executed after applying zonefile changes"`
 	Certificate string `default:"" description:"Certificate file for HTTPS"`
 	PrivateKey  string `default:"" description:"Private key file for HTTPS"`
@@ -29,9 +28,8 @@ func main() {
 	srv := &http.Server{
 		Addr: s.Addr,
 		Handler: handler.New(handler.Config{
-			StaticDir: s.StaticDir,
-			Zonefile:  s.Zonefile,
-			ApplyCmd:  strings.Split(s.ApplyCmd, " "),
+			Zonefile: s.Zonefile,
+			ApplyCmd: strings.Split(s.ApplyCmd, " "),
 		}),
 	}
 	log.Printf("koala %s ready to serve!", version)
