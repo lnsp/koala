635503345854e89fcfd05a2ae1b5bc2ca04ba830
Lennart Espe <lnsp@users.noreply.github.com>
2019-11-11 01:29:34 +0100

    Set app height to 100%

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -6,7 +6,6 @@
 
 <style>
 #app {
-  font-family: -apple-system, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -16,8 +15,10 @@
   background-color: rgba(117,72,232,1) !important;
   border-color: rgba(117,72,232,1) !important;
 }
-html {
+html, body, #app {
   height: 100%;
+  padding: 0;
+  margin: 0;
 }
 body {
   background-color: none;
