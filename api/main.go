24e9dd4f1bc558e58ffe04cfc9c2cd3a3456fcfc
Lennart Espe <lnsp@users.noreply.github.com>
2020-01-19 23:30:39 +0100

    Remove support for standalone certificates

--- api/main.go
+++ api/main.go
@@ -12,14 +12,12 @@ import (
 var version = "dev-build"
 
 type Specification struct {
-	Addr        string `default:":8080" description:"Address the server will be listening on"`
-	Zonefile    string `required:"true" description:"Zonefile to be edited"`
-	ApplyCmd    string `default:"sleep 1" description:"Command executed after applying zonefile changes"`
-	Certificate string `default:"" description:"Certificate file for HTTPS"`
-	PrivateKey  string `default:"" description:"Private key file for HTTPS"`
-	JWTSecret   string `default:"" description:"Auth secret for JWT tokens"`
-	Debug       bool   `default:"false" description:"Enable debug logging" envconfig:"debug"`
-	CORS        bool   `default:"false" description:"Enable support for CORS"`
+	Addr      string `default:":8080" description:"Address the server will be listening on"`
+	Zonefile  string `required:"true" description:"Zonefile to be edited"`
+	ApplyCmd  string `default:"sleep 1" description:"Command executed after applying zonefile changes"`
+	JWTSecret string `default:"" description:"Auth secret for JWT tokens"`
+	Debug     bool   `default:"false" description:"Enable debug logging" envconfig:"debug"`
+	CORS      bool   `default:"false" description:"Enable support for CORS"`
 }
 
 func main() {
@@ -43,17 +41,7 @@ func main() {
 	logrus.WithFields(logrus.Fields{
 		"version": version,
 	}).Info("Up and running")
-	if s.Certificate != "" {
-		logrus.WithFields(logrus.Fields{
-			"cert": s.Certificate,
-			"key":  s.PrivateKey,
-		}).Info("Enabled HTTPS")
-		if err := srv.ListenAndServeTLS(s.Certificate, s.PrivateKey); err != nil {
-			logrus.WithError(err).Fatal("Could not serve")
-		}
-	} else {
-		if err := srv.ListenAndServe(); err != nil {
-			logrus.WithError(err).Fatal("Could not serve")
-		}
+	if err := srv.ListenAndServe(); err != nil {
+		logrus.WithError(err).Fatal("Could not serve")
 	}
 }
