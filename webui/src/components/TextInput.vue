d39db6b35c69ab3b6bd479e4b00d811fc5908313
Lennart Espe <lnsp@users.noreply.github.com>
2020-01-19 19:30:21 +0100

    Add TextInput component

---
+++ webui/src/components/TextInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <input
+    type="text"
+    :placeholder="placeholder"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+    class="py-2 px-4 bg-white focus:outline-none rounded focus:shadow-outline border appearance-none leading-normal border-gray-200 bg-gray-100"
+  />
+</template>
+
+<script>
+export default {
+  name: 'TextInput',
+  props: ['value', 'placeholder'],
+  model: {
+    prop: 'value',
+    event: 'input'
+  }
+}
+</script>
